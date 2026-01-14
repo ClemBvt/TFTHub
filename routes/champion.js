dd9e4c156b8e8f52edd08763de42029ee3607a24
“Clément_Bvt” <clement_brevet@hotmail.com>
2026-01-14 08:09:14 +0100

    Debug CRUD champions

--- routes/champion.js
+++ routes/champion.js
@@ -12,7 +12,19 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/create', (req, res) => {
+router.get('/:id', async (req, res) => {
+    try {
+        const champion = await Champion.findById(req.params.id);
+        if (!champion) {
+            return res.status(404).json({ error: 'Champion not found' });
+        }
+        res.status(200).json(champion);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.get('/pages/create', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'create.html'));
 });
 
@@ -20,7 +32,25 @@ router.post('/', async (req, res) => {
     try {
         const newChampion = new Champion(req.body);
         const championRegistered = await newChampion.save();
-        res.status(200).json(championRegistered);
+        res.status(201).json(championRegistered);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedChampion = await Champion.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(200).json(updatedChampion);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedChampion = await Champion.findByIdAndDelete(req.params.id);
+        res.status(200).json(deletedChampion);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
